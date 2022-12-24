9920debe4fef228dcee9ccc0f81392688ceb77b6
Justus Klausecker <justus@klausecker.de>
2022-12-24 21:55:35 +0100

    filter beginning

--- src/compressor/filter.go
+++ src/compressor/filter.go
@@ -1,13 +1,16 @@
 package compressor
 
-import "bytes"
+import (
+	"bytes"
+	"sort"
+)
 
 const (
-	None = iota
-	Sub
-	Up
-	Average
-	Paeth
+	none = iota
+	sub
+	up
+	average
+	paeth
 )
 
 var (
@@ -15,15 +18,49 @@ var (
 	lookahead = bytes.NewBuffer(make([]byte, 258))
 )
 
-func Filter(row []byte) ([]byte, int) { // returns filtered row and filter index
-	bpp := 
-	switch {
+func sortSlc(row []byte) []byte {
+	sorted := row
+	sort.Slice(sorted, func(i, j int) bool {
+		return sorted[i] < sorted[j]
+	})
+	return sorted
+}
 
+func min(i1, i2 int8) int8 {
+	if i1 > i2 {
+		return i1
 	}
+	return i2
+}
+
+func abs128(b byte) int8 {
+	if b < 128 {
+		return int8(b)
+	}
+	return -int8(b)+127
+}
+
+func absSum(slc []byte, n int) int8 {
+	var sum int8 = 0
+	sum += abs128(slc[0] - slc[1])
+	sum += abs128(slc[n-1] - slc[n-2])
+
+	for i := 1; i < n-1; i++ {
+		sum += min(abs128(slc[i] - slc[i-1]), abs128(slc[i] - slc[i+1]))
+	}
+	return sum
+}
+
+func minAbsDiff(slc []byte) []byte {
+	sorted := sortSlc(slc)
+}
+
+func Filter(row []byte) ([]byte, int) { // returns filtered row and filter index
+	
 }
 
 func subFltr(row []byte, bpp int) []byte {
-	for k, v := range row {
+	for i, b := range row {
 
 	}
 }
