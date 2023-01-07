d745fdd0c92db1a64892d17321b863a94085becc
Justus Klausecker <justus@klausecker.de>
2023-01-07 23:36:38 +0100

    changed build.sh to compile run.go (previously called main.go) in current directory

--- build.sh
+++ build.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 DIR="$(cd "$(dirname "$0")" && pwd)"
-cd $DIR/src
-go build -a main.go
-echo "compiled executable"
 cd $DIR
-cat > run.sh <<EOF
-#!/bin/bash
-
-$DIR/src/main
-EOF
-echo "created run.sh"
-chmod +x run.sh
+go build -a ./src/run.go
+echo "compiled executable"
